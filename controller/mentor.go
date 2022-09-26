8fd1212c4441cfe9a1f04d0edc8652f209998b5c
Jerry Young <jerryyoung2911@gmail.com>
2022-09-26 10:45:14 +0700

    Resolve conflict

--- controller/mentor.go
+++ controller/mentor.go
@@ -6,21 +6,8 @@ type MentorController struct {
 	Model model.MentorModel
 }
 
-<<<<<<< HEAD
-func (mc MentorController) GetAll([]model.Mentor, error) {
-	// Ini Get All
-	result, err := mc.Model.GetAll()
-	if err != nil {
-		return nil, err
-	}
-=======
 func (mc *MentorController) GetAllMentor() ([]model.Mentor, error) {
 	var result []model.Mentor
-<<<<<<< HEAD
->>>>>>> GetAllMentor Controller
-=======
-
->>>>>>> GetAllMentor 1
 	return result, nil
 }
 
